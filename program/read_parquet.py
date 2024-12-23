0bb61cd0161e9e64f40aafde89c5ee4cc7e7066f
anannnchim <nachimchim@gmail.com>
2024-12-24 06:22:13 +0700

    Finish Initialize data

--- program/read_parquet.py
+++ program/read_parquet.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from sysinit.futures.barchart_futures_contract_prices import strip_file_names
+
 if __name__ == '__main__':
     a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/spotfx_prices/AUDUSD.parquet")
     print(a)
