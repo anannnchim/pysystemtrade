7541aa17c03d3e69143dc56eb863dbd3c3be68fb
anannnchim <nachimchim@gmail.com>
2024-12-24 18:22:50 +0700

    odified helper function

--- program/setup_strategy.py
+++ program/setup_strategy.py
@@ -3,5 +3,12 @@ import pandas as pd
 if __name__ == '__main__':
 
     # 1. Global capital
-    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/capital/__global_capital.parquet")
-    print(a)
+    # a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/capital/__global_capital.parquet")
+    # print(a)
+
+    # a = pd.read_parquet("/Users/nanthawat/PycharmProjects/private-pysystemtrade/data/parquet/capital/strategy_1.parquet")
+    # print(a)
+
+    b = pd.read_parquet(
+        "/Users/nanthawat/PycharmProjects/private-pysystemtrade/data/parquet/capital/strategy_1.parquet")
+    print(b)
