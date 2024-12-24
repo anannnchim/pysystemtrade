9611a0af816a6c52e68268cad74d35fd74916785
anannnchim <nachimchim@gmail.com>
2024-12-24 07:49:25 +0700

    finish setup linux script

---
+++ program/initialize/convert_csv_to_parquet.py
@@ -0,0 +1,47 @@
+from sysdata.config.production_config import get_production_config
+from syscore.fileutils import resolve_path_and_filename_for_package
+from sysdata.csv.csv_futures_contract_prices import ConfigCsvFuturesPrices, csvFuturesContractPriceData
+from sysinit.futures.contract_prices_from_split_freq_csv_to_db import (
+    init_db_with_split_freq_csv_prices_for_code,
+)
+
+# Config for the Barchart CSV files
+BARCHART_CONFIG = ConfigCsvFuturesPrices(
+    input_date_index_name="Time",  # Matches the CSV header
+    input_skiprows=0,  # No rows to skip
+    input_skipfooter=0,  # No footer rows to skip
+    input_date_format="%Y-%m-%dT%H:%M:%S",
+    input_column_mapping=dict(
+        OPEN="Open", HIGH="High", LOW="Low", FINAL="Close", VOLUME="Volume"
+    ),
+)
+
+# Resolve the path to the CSV files
+# datapath = resolve_path_and_filename_for_package(
+#     get_production_config().get_element_or_default("barchart_path", None)
+# )
+#
+
+if __name__ == '__main__':
+    """
+    This is the newest version for converting csv to parquet. 
+    """
+    datapath = '/Users/nanthawat/PycharmProjects/bc-utils/data'
+
+    # Note 1. Convert single instrument
+    # Import prices for a single instrument (CORN)
+    init_db_with_split_freq_csv_prices_for_code("CORN_mini", datapath=datapath, csv_config=BARCHART_CONFIG)
+
+    # Note 2. Convert the whole folder
+    # csv_prices = csvFuturesContractPriceData(datapath)
+    # instrument_codes = csv_prices.get_list_of_instrument_codes_with_merged_price_data()
+    # instrument_codes.sort()
+    # for instrument_code in instrument_codes:
+    #     print(instrument_code)
+    #     init_db_with_split_freq_csv_prices_for_code(
+    #         instrument_code,
+    #         datapath,
+    #         csv_config=BARCHART_CONFIG,
+    #         ignore_duplication=True,  # if True, we overwrite existing prices
+    #     )
+
