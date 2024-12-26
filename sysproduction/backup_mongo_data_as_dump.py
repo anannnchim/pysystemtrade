32e3dadf8ed5b0ce535fecea17ab7e16e541c6ab
anannnchim <nachimchim@gmail.com>
2024-12-26 08:44:16 +0700

    modified backup mongo files

--- sysproduction/backup_mongo_data_as_dump.py
+++ sysproduction/backup_mongo_data_as_dump.py
@@ -29,16 +29,31 @@ class backupMongo(object):
         log.debug("Copying data to backup destination")
         backup_mongo_dump(data)
 
+# def dump_mongo_data(data: dataBlob):
+#     config = data.config
+#     host = config.get_element_or_arg_not_supplied("mongo_host")
+#     path = get_mongo_dump_directory()
+#     data.log.debug("Dumping mongo data to %s NOT TESTED IN WINDOWS" % path)
+#     if host.startswith("mongodb://"):
+#         os.system("mongodump --uri='%s' -o=%s" % (host, path))
+#     else:
+#         os.system("mongodump --host='%s' -o=%s" % (host, path))
+#     data.log.debug("Dumped")
 
+
+# CHAT FUNCTION: Modified for URI since I use mongoDB atlas
 def dump_mongo_data(data: dataBlob):
     config = data.config
     host = config.get_element_or_arg_not_supplied("mongo_host")
     path = get_mongo_dump_directory()
     data.log.debug("Dumping mongo data to %s NOT TESTED IN WINDOWS" % path)
-    if host.startswith("mongodb://"):
+
+    # Check for both 'mongodb://' and 'mongodb+srv://'
+    if host.startswith("mongodb://") or host.startswith("mongodb+srv://"):
         os.system("mongodump --uri='%s' -o=%s" % (host, path))
     else:
         os.system("mongodump --host='%s' -o=%s" % (host, path))
+
     data.log.debug("Dumped")
 
 
@@ -50,4 +65,21 @@ def backup_mongo_dump(data):
 
 
 if __name__ == "__main__":
+    """
+    This will:
+    1. mongodump (get data from mongoDB) and put it in /data/mongo_dump
+    mongo_dump_directory: private-pysystemtrade/data/mongo_dump
+    
+    2. Then copy that `data/mongo_dump' to 
+    /Users/nanthawat/PycharmProjects/private-pysystemtrade/backup/mongo
+    
+    (OR)
+    mongodump --uri="mongodb+srv://USERNAME:PASSWORD@production.idynt.mongodb.net/?retryWrites=true&w=majority&appName=production" --out=/Users/nanthawat/PycharmProjects/private-pysystemtrade/data/mongo_dump
+    
+    (OR)
+    source /Users/nanthawat/PycharmProjects/pysystemtrade/project_env.sh    
+    python3 /Users/nanthawat/PycharmProjects/pysystemtrade/sysproduction/backup_mongo_data_as_dump.py
+    """
+    os.environ["PATH"] += ":/usr/local/mongodb-tools/bin"
     backup_mongo_data_as_dump()
+
