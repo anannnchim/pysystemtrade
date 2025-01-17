a3cb2ebb55869e894d435f7142cacd089164c4e0
anannnchim <nachimchim@gmail.com>
2025-01-17 13:33:34 +0700

    add helper fule

--- program/check_capital_data.py
+++ program/check_capital_data.py
@@ -15,25 +15,25 @@ if __name__ == '__main__':
     a = pd.read_parquet("/Users/nanthawat/PycharmProjects/private-pysystemtrade/data/parquet/capital/__global_capital.parquet")
     print(a)
 
-    print("2. Strategy_1 --------------------------------")
-    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/private-pysystemtrade/data/parquet/capital/strategy_1.parquet")
+    print("2. system_01 --------------------------------")
+    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/private-pysystemtrade/data/parquet/capital/system_01.parquet")
     print(a)
 
     # print("3. Backtesting data ------------------------------")
-    # file_path = '/Users/nanthawat/PycharmProjects/pysystemtrade/private/backtests/strategy_1/20250102_110049_backtest.pck'
+    # file_path = '/Users/nanthawat/PycharmProjects/pysystemtrade/private/backtests/system_01/20250102_110049_backtest.pck'
     # with open(file_path, 'rb') as file:
     #     backtest_data = pickle.load(file)
     # print(backtest_data)
 
     print("4. Optimal position --------------------------------")
-    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/optimal_positions/strategy_1 CORN.parquet")
+    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/optimal_positions/system_01 ETHER-micro.parquet")
     print(a)
 
     print("5. Contract position --------------------------------")
-    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/CORN#20251200.parquet")
-    print(a)
+    # a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/ETHER-micro#20251200.parquet")
+    # print(a)
 
     print("5. Strategy position --------------------------------")
-    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/strategy_positions/strategy_1 CORN.parquet")
-    print(a)
+    # a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/strategy_positions/system_01 ETHER-micro.parquet")
+    # print(a)
 
