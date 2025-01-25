712fc923c02583dc53e2bc0164b4b9c63f66a6b0
anannnchim <nachimchim@gmail.com>
2025-01-25 22:15:23 +0700

    init EUR_micro data

--- program/read_parquet.py
+++ program/read_parquet.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     # Adjust pandas options to display all rows and columns
     pd.set_option('display.max_columns', None)  # Show all columns
-    pd.set_option('display.max_rows', None)  # Show all rows
+    # pd.set_option('display.max_rows', None)  # Show all rows
     pd.set_option('display.expand_frame_repr', False)  # Prevent wrapping to new lines
 
-    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@ETHER-micro#20250200.parquet")
+    a = pd.read_parquet("/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_multiple_prices/EUR_micro.parquet")
     print(a)
-    a.to_csv("/Users/nanthawat/PycharmProjects/pysystemtrade/program/output_csv/price.csv")
+
+    # a.to_csv("/Users/nanthawat/PycharmProjects/pysystemtrade/program/output_csv/price.csv")
