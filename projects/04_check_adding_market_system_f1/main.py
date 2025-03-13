351ef8cbad17d108065a2ef808b3daf58b2c8093
anannnchim <nachimchim@gmail.com>
2025-03-14 10:21:26 +1100

    Update dt

---
+++ projects/04_check_adding_market_system_f1/main.py
@@ -0,0 +1,18 @@
+from program.googlesheet.update_system_gg import update_market_monitoring, update_portfolio_monitoring, \
+    update_system_verification
+from program.helper.run_scripts import run_scripts
+from sysdata.config.configdata import Config
+from sysdata.sim.csv_futures_sim_data import csvFuturesSimData
+from sysdata.sim.db_futures_sim_data import dbFuturesSimData
+from systems.provided.futures_chapter15.basesystem import futures_system
+CONFIG_PATH = "/Users/nanthawat/PycharmProjects/pysystemtrade/projects/04_check_adding_market_system_f1/config.yaml"
+config = Config(CONFIG_PATH)
+data = csvFuturesSimData()
+
+s = futures_system(config=config, data=data)
+
+if __name__ == '__main__':
+    a = s.rawdata.get_daily_percentage_volatility("EUR_micro") * 16  # Daily % risk or (number%)
+    print(a)
+
+
