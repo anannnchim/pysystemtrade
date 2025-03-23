65d2150d38162c53ddba42c7a41b0962dbf1949c
anannnchim <nachimchim@gmail.com>
2025-03-23 08:05:48 +0700

    update dt

--- program/others/read_parquet.py
+++ program/others/read_parquet.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     # print(a)
 
     # Contract price
-    path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@VIX_mini#20250600.parquet"
+    path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/optimal_positions/system_01 VIX_mini.parquet"
     a = pd.read_parquet(path)
     print(a)
 
