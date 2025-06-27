3640b98080b83eebadcea1fffd6f5d5a5d2e0ab1
anannnchim <nachimchim@gmail.com>
2025-06-27 08:33:53 +0700

    update dt

--- projects/08_system_diagonostic/main.py
+++ projects/08_system_diagonostic/main.py
@@ -10,6 +10,7 @@ from systems.provided.futures_chapter15.basesystem import futures_system
 data = csvFuturesSimData()
 
 config = Config("/Users/nanthawat/PycharmProjects/pysystemtrade/projects/01_running_system_f1/config.yaml")
+config = Config("/Users/nanthawat/PycharmProjects/pysystemtrade/projects/run_backtest/system_f1_new_config.yaml")
 
 s = futures_system(config=config, data=data)
 
