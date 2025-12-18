e0f454dd7702e9329e0bd29eeea8c7b1b44ddeeb
anannnchim <nachimchim@gmail.com>
2025-12-18 07:28:57 +0700

    Latest data before adding new market

--- program/others/read_parquet.py
+++ program/others/read_parquet.py
@@ -6,7 +6,7 @@ from sysproduction.data.capital import dataCapital
 from sysproduction.data.positions import diagPositions
 
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_rows', None)
 pd.set_option('display.expand_frame_repr', False)
 
 if __name__ == '__main__':
@@ -39,10 +39,10 @@ if __name__ == '__main__':
     # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_multiple_prices/SOYMEAL.parquet"
     # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/strategy_positions/system_01 JGB-SGX-mini.parquet"
     # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/JGB-SGX-mini#20251200.parquet"
-    path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/EU-BANKS#20251200.parquet"
+    path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_multiple_prices/EU-BANKS.parquet"
     # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/JGB-SGX-mini#20260300.parquet"
     # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@SILVER#20260300.parquet"
-    a = pd.read_parquet(path)
+    a = pd.read_parquet(path).tail(20)
     print(a)
     # a.plot()
     # plt.show()
