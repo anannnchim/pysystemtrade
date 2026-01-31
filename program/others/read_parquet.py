6357b75662bfd94f057eb95b0ceded701fd2cdd3
anan <nachimchim@gmail.com>
2026-01-31 10:58:36 +0700

    update data

--- program/others/read_parquet.py
+++ program/others/read_parquet.py
@@ -11,51 +11,7 @@ pd.set_option('display.expand_frame_repr', False)
 
 if __name__ == '__main__':
 
-    # Note - Print data
 
-    # Global capital
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/capital/__global_capital.parquet"
-    # a = pd.read_parquet(path)
-    # print(a)
-    #
-    # # System capital
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/capital/system_01.parquet"
-    # a = pd.read_parquet(path)
-    # print(a)
-
-    # Optimal position
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/optimal_positions/system_01 VIX_mini.parquet"
-    # a = pd.read_parquet(path)
-    # print(a)
-
-    # Contract price
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/capital/system_01.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/optimal_positions/system_01 MUMMY.parquet"
-    # a = pd.read_parquet(path)
-    # print(a)
-
-
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_multiple_prices/SILVER.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_multiple_prices/SOYMEAL.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/strategy_positions/system_01 JGB-SGX-mini.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/JGB-SGX-mini#20251200.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_adjusted_prices/BTP3.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/contract_positions/JGB-SGX-mini#20260300.parquet"
-    # path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@SILVER#20260300.parquet"
-    # a = pd.read_parquet(path).tail(20)
-    # print(a)
-    # a.plot()
-    # plt.show()
-
-    '''
-    Spread 
-    1. Config: 0.064 (Rob change to 0.048)
-    2. Parquet min: 0.07 
-    3. Live (21.43): AUG,SEP = 0.02,0.04 19.44
-    
- 
-    '''
-    # /Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/optimal_positions/system_01 MUMMY.parquet
     # # # # # Note - Delete some row
     # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/capital/__global_capital.parquet"
     # df = pd.read_parquet(file_path, engine="pyarrow")  # or engine="fastparquet"
@@ -64,18 +20,13 @@ if __name__ == '__main__':
     # print(df)
 
     # Note - Delate second last row in capital
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@MXP#20260300.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@WHEAT_mini#20260300.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@CRUDE_W_micro#20260400.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@GOLD_micro#20260400.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@EU-BANKS#20260300.parquet"
-    file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@WHEAT_mini#20260300.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_adjusted_prices/MUMMY.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@MUMMY#20260300.parquet"
-    # file_path = "/Users/nanthawat/PycharmProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@IRON#20260400.parquet"
+
+    file_path = "/home/anan/AnanProjects/pysystemtrade/data/parquet/futures_contract_prices/Day@PLAT#20261000.parquet"
+    file_path = "/home/anan/AnanProjects/pysystemtrade/data/parquet/futures_adjusted_prices/PLAT.parquet"
+    # file_path = "/home/anan/AnanProjects/pysystemtrade/data/parquet/futures_multiple_prices/PLAT.parquet"
+    # file_path = "/home/anan/AnanProjects/pysystemtrade/data/parquet/futures_contract_prices/Dayay@EUR_micro#20260300.parquet"
+
     df = pd.read_parquet(file_path, engine="pyarrow")  # or engine="fastparquet"
-    # df = df.drop(df.index[-2])
-    # df.to_parquet(file_path, index=True, engine="pyarrow")
     print(df)
     df.plot()
     plt.show()
