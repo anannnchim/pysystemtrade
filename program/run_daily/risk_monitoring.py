cca80a8d34ac4a0eb0c216d3e204e8ae148a1264
anan <nachimchim@gmail.com>
2026-02-12 12:04:30 +0700

    update data

--- program/run_daily/risk_monitoring.py
+++ program/run_daily/risk_monitoring.py
@@ -46,11 +46,11 @@ from sysdata.sim.db_futures_sim_data import dbFuturesSimData
 from systems.provided.futures_chapter15.basesystem import futures_system
 from systems.risk_overlay import get_risk_multiplier
 
-# CONFIG_PATH = "/Users/nanthawat/PycharmProjects/pysystemtrade/projects/01_running_system_f1/config.yaml"
-CONFIG_PATH = "/home/anan/AnanProjects/pysystemtrade/private/systems/system_01/config.yaml"
+CONFIG_PATH = "/home/anan/AnanProjects/pysystemtrade/private/systems/system_f1/private_config.yaml"
+# CONFIG_PATH = "/home/anan/AnanProjects/pysystemtrade/private/systems/system_01/config.yaml"
 config = Config(CONFIG_PATH)
-# data = csvFuturesSimData()
-data = dbFuturesSimData()
+data = csvFuturesSimData()
+# data = dbFuturesSimData()
 
 s = futures_system(config=config, data=data)
 
