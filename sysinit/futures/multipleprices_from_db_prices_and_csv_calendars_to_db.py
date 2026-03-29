06e564808ea7abaec5ef5ebe1d5238edc6dcf075
anan <nachimchim@gmail.com>
2026-03-29 10:39:57 +0700

    init two inst

--- sysinit/futures/multipleprices_from_db_prices_and_csv_calendars_to_db.py
+++ sysinit/futures/multipleprices_from_db_prices_and_csv_calendars_to_db.py
@@ -221,7 +221,7 @@ if __name__ == "__main__":
     # )
 
     process_multiple_prices_single_instrument(
-        instrument_code="S50-TFEX",
+        instrument_code="BOBL",
         csv_multiple_data_path=csv_multiple_data_path,
         csv_roll_data_path=csv_roll_data_path,
         ADD_TO_DB=True,
